1e3608c89736c23dfbbc13f5d0a0a8881a23aecc
edwin4v@gmail.com <edwin4v@gmail.com>
2021-06-21 23:35:31 +0800

    Added evaluation script for iou by class

--- utils.py
+++ utils.py
@@ -1,13 +1,20 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 def visualize(**images):
     n = len(images)
     plt.figure(figsize=(16, 5))
     for i, (name, image) in enumerate(images.items()):
         plt.subplot(1, n, i + 1)
-        plt.title(name + "_" + label.lower())
+        plt.title(name)
         plt.xticks([])
         plt.yticks([])
         plt.imshow(image)
         
     plt.show()
+    
+def iou(gt_mask, pred_mask):
+    intersection = np.logical_and(gt_mask, pred_mask)
+    union = np.logical_or(gt_mask, pred_mask)
+    
+    return np.sum(intersection) / np.sum(union)
